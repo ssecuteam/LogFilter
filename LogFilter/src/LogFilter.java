6058326922af3acf70059fd81c767077f41c7c39
m.kujawa <m.kujawa@amdn228.DIGITAL.local>
2013-01-18 13:08:42 +0100

    trzecia linia

--- LogFilter/src/LogFilter.java
+++ LogFilter/src/LogFilter.java
@@ -3,5 +3,6 @@ public class LogFilter {
 	public static void main(String[] args) {
 		System.out.println("test git");
 		System.out.println("test git druga linijka");	
+		System.out.println("test git trzecia linijka");	
   }
 }
